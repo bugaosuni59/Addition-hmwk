989b83a0ce70c711ed6ee640040e260881c3487d
bugaosuni59 <598586484@qq.com>
2017-04-11 01:04:59 +0800

    0

---
+++ pdf-fs/getBat3.cpp
@@ -0,0 +1,24 @@
+//py -2 pd2.py -o output.html fs_abba.pdf
+#include <bits/stdc++.h>
+using namespace std;
+
+string s1="py -2 pd2.py -o ";
+string s2;
+string s3=".xml ";
+string s4;
+//string s;
+
+int main(){
+	freopen("a.txt","r",stdin);
+	freopen("change3.bat","w",stdout);
+	int len;
+	while(cin>>s4){
+		len=s4.length();
+		s2="";
+		for(int i=0;i<len-4;i++)s2+=s4[i];
+		cout<<s1+s2+s3+s4<<endl;
+	}
+	
+	return 0;	
+}
+
